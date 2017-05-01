c6555a7b4f0193d6903575f6c20f1a5d37160520
Koala633 <flow747@yahoo.fr>
2017-05-01 13:20:53 +0200

    Update hostbase.sh

--- hostbase/hostbase.sh
+++ hostbase/hostbase.sh
@@ -444,7 +444,7 @@ ddns-updates off;
 ddns-update-style interim;
 authoritative;
 subnet 10.0.0.0 netmask 255.255.255.0 {
-interface wlan0;
+interface ${monmode};
 range 10.0.0.2 10.0.0.10;
 option routers 10.0.0.1;
 option subnet-mask 255.255.255.0;
