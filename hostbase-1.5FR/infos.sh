fdbdee0d3cad182987f83c2b4c60ce3dbddb352a
koala <thelonewolf633@gmail.com>
2022-10-20 19:17:48 +0200

    delete older versions

--- hostbase-1.5FR/infos.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-
-echo -e "\e[1;31m [!] AVERTISSEMENT: le wifi de la box ciblée a été désactivé temporairement... pas de panique l'attaque reprendra automatiquement d'ici 10 minutes... vous pouvez fermer cette fenetre (ATTENTION: pas la fenetre ou a été lancé hostbase, juste celle-ci).Si jamais cette fenetre vient a apparaitre plusieurs fois malgré la relance de l'attaque, c'est que la personne a probablement coupé son wifi ou eteinds sa box... dans ce cas continuer l'attaque ne sert a rien vous pouvez taper ctrl+c dans la console ou a été lancé hostbase pour arréter l'attaque.\e[0m"
