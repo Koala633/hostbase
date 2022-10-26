f853c118b211c00aa8998b7b13e2fa97eddf65d2
koala <thelonewolf633@gmail.com>
2022-10-26 21:30:32 +0200

    delete older versions

--- hostbase-1.6FR/accessdos.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-
-
-File.open("cartedos.txt").readlines.each do |cartedos|
-   puts cartedos
-$cartedos = cartedos.chomp
-end
--- hostbase-1.6FR/infos2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo -e "\e[1;32m [!] un réseau wifi vient d'apparaitre.ATTENTION: plus le réseau est loin moins vous aurez de chance que l'attaque fonctionne.Vérifiez la colonne PWR dans la fenetre airodump-ng du scan de réseau pour vous faire une idée de la distance du réseau ciblé.Fermez cette fenetre et relancer hostbase dans la console principale avec: ruby hostbase.rb\e[0m"
- 
-
