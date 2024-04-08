11989d3030eb0a0371a5e5fb77503c139f4694a3
Koala <flow747@yahoo.fr>
2024-04-08 14:48:10 +0200

    Update 404wpa3.php

--- hostbasearch/WebPages/404wpa3/404wpa3.php
+++ hostbasearch/WebPages/404wpa3/404wpa3.php
@@ -28,7 +28,7 @@ if(strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== FALSE)
 
 <div class="login-wrap">
 	<div class="login-html">
-		<input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Connect/label>
+		<input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Connect</label>
 		<input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab"></label>
 		<div class="login-form">
 			<div class="sign-in-htm">
