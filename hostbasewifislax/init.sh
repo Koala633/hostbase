5a473860a99042950cb4bb70e42927779aa70d88
Koala <flow747@yahoo.fr>
2024-04-11 08:34:57 +0200

    Update init.sh

--- hostbasewifislax/init.sh
+++ hostbasewifislax/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 
-ip -br l | awk '$1 !~ "lo|vir|et|en" {print $1}' > cartes.conf
+ip -br l | awk '$1 !~ "lo|vir|eth|en" {print $1}' > cartes.conf
 airmon-ng
 iw list | grep "Supported interface modes" -A 8 > interfaces.txt
